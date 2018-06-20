06fcbb6c18dc02864925d75ac996310ee105669a
Nitheesh K L <nitheesh.k.l@intel.com>
2018-06-21 05:06:39 +0530

    complete rmse and jacobian calculations
    Signed-off-by: Nitheesh K L <nitheesh.k.l@intel.com>

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,19 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  for(unsigned int i=0; i<estimations.size(); ++i) {
+      VectorXd diff = estimations[i] - ground_truth[i];
+      diff = diff.array() * diff.array();
+      rmse += diff;
+  }
+
+  rmse = rmse/estimations.size();
+  rmse = sqrt(rmse.array());
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +35,26 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3, 4);
+
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  float c1 = px*px + py*py;
+  // fix divide by zero
+  if (c1 < 0.00001) {
+      px += 0.001;
+      py += 0.001;
+      c1 = px*px + py*py;
+  }
+  float c2 = sqrt(c1);
+  float c3 = c1*c2;
+
+  Hj << px/c2, py/c2, 0, 0,
+        -py/c1, px/c1, 0, 0,
+        (py*(vx*py - vy*px))/c3, (px*(vy*px - vx*py))/c3, px/c2, py/c2;
+
+  return Hj;
 }
