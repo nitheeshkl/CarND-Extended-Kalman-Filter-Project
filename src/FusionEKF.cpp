e348e8b3f55a82549d1dc98b0c74c7cc9000faea
Nitheesh K L <nitheesh.k.l@intel.com>
2018-06-21 04:34:00 +0530

    initialize FusionEKF
    Signed-off-by: Nitheesh K L <nitheesh.k.l@intel.com>

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,7 +36,12 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
 
+  // acceleration in x & Y direction considered as noise
+  noise_ax = 9.0;
+  noise_ay = 9.0;
 
 }
 
@@ -58,21 +63,52 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+    cout << "initializing..." << endl;
+
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
+    VectorXd x(4);
+
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      float rho = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rhodot = measurement_pack.raw_measurements_[2];
+      x << rho*cos(phi), rho*sin(phi), rhodot*cos(phi), rhodot*sin(phi);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      x << measurement_pack.raw_measurements_[0],
+          measurement_pack.raw_measurements_[1], 0.0f, 0.0f;
     }
+
+    previous_timestamp_ = measurement_pack.timestamp_;
+
+    // Initialize P with a large covariance for vx & vy since their values are
+    // highly uncertain at this point in time.
+    MatrixXd p(4, 4);
+    p << 1, 0, 0, 0,
+         0, 1, 0, 0,
+         0, 0, 1000, 0,
+         0, 0, 0, 1000;
+
+    MatrixXd f(4, 4);
+    f << 1, 0, 0, 0,
+         0, 1, 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1;
+
+    MatrixXd Q(4, 4);
+
+    // initialize EKF
+    ekf_.Init(x, p, f, H_laser_, R_laser_, Q);
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
